ace42683a8663462e8d76ac25cf4432e88a6391d
Mark Goadrich <goadrich@hendrix.edu>
2023-06-15 14:05:01 -0500

    Rounding the corner, and adding bold weight to the title

--- hendrix_today_app/lib/Widgets/event_dialog.dart
+++ hendrix_today_app/lib/Widgets/event_dialog.dart
@@ -12,7 +12,10 @@ class EventDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return AlertDialog(
       scrollable: true,
-      title: Text(event.title.toString()),
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(24.0))),
+      title: Text(event.title.toString(),
+          style: const TextStyle(fontWeight: FontWeight.bold)),
       insetPadding: const EdgeInsets.symmetric(vertical: 100, horizontal: 50),
       content: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
