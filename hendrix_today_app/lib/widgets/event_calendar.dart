6874899e53e4e2e3268e14ba78186084d7726ef2
Mark Goadrich <goadrich@hendrix.edu>
2023-06-16 16:02:27 -0500

    Fixing Calendar scrolling issues, updating in pubspec

--- hendrix_today_app/lib/widgets/event_calendar.dart
+++ hendrix_today_app/lib/widgets/event_calendar.dart
@@ -18,7 +18,6 @@ class EventCalendar extends StatefulWidget {
 //code obtained from TableCalendar repo: https://github.com/aleksanderwozniak/table_calendar
 class _EventCalendarState extends State<EventCalendar> {
   late final ValueNotifier<List<Event>> _selectedEvents;
-  CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime calendarRoot = DateTime.now();
   DateTime _selectedDay = DateTime.now();
@@ -36,9 +35,10 @@ class _EventCalendarState extends State<EventCalendar> {
   }
 
   List<Event> _getEventsForDay(DateTime day) {
-    return Provider.of<AppState>(context, listen: false).events
-      .where((event) => event.matchesDate(day))
-      .toList();
+    return Provider.of<AppState>(context, listen: false)
+        .events
+        .where((event) => event.matchesDate(day))
+        .toList();
   }
 
   void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
@@ -55,8 +55,8 @@ class _EventCalendarState extends State<EventCalendar> {
   @override
   Widget build(BuildContext context) {
     final today = DateTime.now();
-    final calendarStartDate = DateTime(today.year - 1, today.month, today.day);
-    final calendarEndDate = DateTime(today.year + 1, today.month, today.day);
+    final calendarStartDate = DateTime(today.year - 2, today.month, today.day);
+    final calendarEndDate = DateTime(today.year, today.month + 1, today.day);
     return SizedBox(
       child: Consumer<AppState>(
         builder: (context, appState, _) {
@@ -66,7 +66,10 @@ class _EventCalendarState extends State<EventCalendar> {
                 firstDay: calendarStartDate,
                 lastDay: calendarEndDate,
                 focusedDay: _focusedDay,
-                calendarFormat: _calendarFormat,
+                calendarFormat: CalendarFormat.month,
+                availableCalendarFormats: const {
+                  CalendarFormat.month: 'Month',
+                },
                 eventLoader: _getEventsForDay,
                 selectedDayPredicate: (day) {
                   // Use `selectedDayPredicate` to determine which day is currently selected.
@@ -76,17 +79,13 @@ class _EventCalendarState extends State<EventCalendar> {
                   return isSameDay(_selectedDay, day);
                 },
                 onDaySelected: _onDaySelected,
-                onFormatChanged: (format) {
-                  if (_calendarFormat != format) {
-                    // Call `setState()` when updating calendar format
-                    setState(() {
-                      _calendarFormat = format;
-                    });
-                  }
-                },
                 onPageChanged: (focusedDay) {
                   // No need to call `setState()` here
+                  _selectedDay = DateTime(
+                      focusedDay.year, focusedDay.month, _selectedDay.day);
                   _focusedDay = focusedDay;
+                  _selectedEvents.value = _getEventsForDay(_selectedDay);
+                  setState(() {});
                 },
               ),
               //EventList()
