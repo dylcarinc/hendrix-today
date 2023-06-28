577b1e85109df1e04ad81eba79e1480e4024fd34
Simon <reidst@hendrix.edu>
2023-06-28 09:35:49 -0500

    Fix formatting after merge

--- hendrix_today_app/lib/screens/resource_screen.dart
+++ hendrix_today_app/lib/screens/resource_screen.dart
@@ -40,31 +40,31 @@ class _ResourcesScreenState extends State<ResourcesScreen> {
       body: Center(
         child: Column(
           children: <Widget>[
-      const SizedBox(height: 80),
-      const ResourceButton(
-          titleString: "submit new event",
-          icon: Icons.add,
-          color: Color.fromARGB(255, 202, 81, 39),
-          url:
-              'https://forms.office.com/Pages/ResponsePage.aspx?id=jMH2DNLQP0qD0GY9Ygpj020T9lhtzfhCi8WBPrgNg0xURFZXMEEyUzUwR0lNSzZTTDdWWEQwOERSWiQlQCN0PWcu'),
-      const SizedBox(height: 30),
-      ResourceButton(
-          titleString: "caf menu today",
-          icon: Icons.food_bank,
-          color: Colors.black,
-          url: menuLink),
-      const SizedBox(height: 30),
-      const ResourceButton(
-          titleString: "public safety",
-          icon: Icons.phone,
-          color: Color.fromARGB(255, 202, 81, 39),
-          url: "tel:+5014507711"),
-      const SizedBox(height: 30),
-      const ResourceButton(
-          titleString: "campus map",
-          icon: Icons.map,
-          color: Colors.grey,
-          url: "https://www.hendrix.edu/campusmap/"),
+            const SizedBox(height: 80),
+            const ResourceButton(
+                titleString: "submit new event",
+                icon: Icons.add,
+                color: Color.fromARGB(255, 202, 81, 39),
+                url:
+                    'https://forms.office.com/Pages/ResponsePage.aspx?id=jMH2DNLQP0qD0GY9Ygpj020T9lhtzfhCi8WBPrgNg0xURFZXMEEyUzUwR0lNSzZTTDdWWEQwOERSWiQlQCN0PWcu'),
+            const SizedBox(height: 30),
+            ResourceButton(
+                titleString: "caf menu today",
+                icon: Icons.food_bank,
+                color: Colors.black,
+                url: menuLink),
+            const SizedBox(height: 30),
+            const ResourceButton(
+                titleString: "public safety",
+                icon: Icons.phone,
+                color: Color.fromARGB(255, 202, 81, 39),
+                url: "tel:+5014507711"),
+            const SizedBox(height: 30),
+            const ResourceButton(
+                titleString: "campus map",
+                icon: Icons.map,
+                color: Colors.grey,
+                url: "https://www.hendrix.edu/campusmap/"),
           ],
         ),
       ),
