90e0b95df9ec67ba8ef7442340526cc49e117ff3
reidst <reidst@hendrix.edu>
2023-06-30 13:22:45 -0500

    Removed unnecessary `Consumer` widget

--- hendrix_today_app/lib/widgets/event_list.dart
+++ hendrix_today_app/lib/widgets/event_list.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:hendrix_today_app/objects/app_state.dart';
 import 'package:hendrix_today_app/objects/event.dart';
 import 'package:hendrix_today_app/widgets/event_card.dart';
-
-import 'package:provider/provider.dart';
 
 class EventList extends StatelessWidget {
   const EventList({super.key, required this.events});
@@ -12,17 +9,13 @@ class EventList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<AppState>(
-      builder: (context, appState, child) {
-        return ListView(
-          shrinkWrap: true,
-          physics: const ScrollPhysics(),
-          children: [
-            for (Event e in events) EventCard(event: e),
-            const SizedBox(height: 85),
-          ],
-        );
-      },
+    return ListView(
+      shrinkWrap: true,
+      physics: const ScrollPhysics(),
+      children: [
+        for (Event e in events) EventCard(event: e),
+        const SizedBox(height: 85),
+      ],
     );
   }
 }
