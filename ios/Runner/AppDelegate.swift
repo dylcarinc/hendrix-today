dae80aef552a609df32d176e3641e76ed3dda0e4
Jonathon Qualls <jonathonq02@gmail.com>
2023-11-01 15:56:40 -0500

    NOTIFICATIONS WORK

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,62 +1,46 @@
 import UIKit
-import Firebase
-import FirebaseCore
 import FirebaseMessaging
 import Flutter
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions
-      launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    
-    UNUserNotificationCenter.current().delegate = self
-    let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-    UNUserNotificationCenter.current().requestAuthorization(
-        options: authOptions) { _, _ in }
-    application.registerForRemoteNotifications()
-    Messaging.messaging().delegate = self
-    return true
+      UNUserNotificationCenter.current().delegate = self
+          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+          UNUserNotificationCenter.current().requestAuthorization(
+              options: authOptions) { _, _ in }
+          application.registerForRemoteNotifications()
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-}
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    func userNotificationCenter(
-        _ center: UNUserNotificationCenter,
-        willPresent notification: UNNotification,
-        withCompletionHandler completionHandler:
-        @escaping (UNNotificationPresentationOptions) -> Void
-        ) {
-          if #available(iOS 14.0, *) {
-              completionHandler([[.banner, .sound]])
-          } else {
-              // Fallback on earlier versions
-          }
-    }
+    override func userNotificationCenter(
+            _ center: UNUserNotificationCenter,
+            willPresent notification: UNNotification,
+            withCompletionHandler completionHandler:
+            @escaping (UNNotificationPresentationOptions) -> Void
+            ) {
+              if #available(iOS 14.0, *) {
+                  completionHandler([[.banner, .sound]])
+              } else {
+                  // Fallback on earlier versions
+              }
+        }
 
-    func userNotificationCenter(
-        _ center: UNUserNotificationCenter,
-        didReceive response: UNNotificationResponse,
-        withCompletionHandler completionHandler: @escaping () -> Void
-      ) {
-        completionHandler()
-      }
-    func application(
-      _ application: UIApplication,
-      didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-    ) {
-      Messaging.messaging().apnsToken = deviceToken
-    }
-}
-extension AppDelegate: MessagingDelegate {
-  func messaging(
-    _ messaging: Messaging,
-    didReceiveRegistrationToken fcmToken: String?
-  ) {
-    let tokenDict = ["token": fcmToken ?? ""]
-    NotificationCenter.default.post(
-      name: Notification.Name("FCMToken"),
-      object: nil,
-      userInfo: tokenDict)
-  }
+    override func userNotificationCenter(
+            _ center: UNUserNotificationCenter,
+            didReceive response: UNNotificationResponse,
+            withCompletionHandler completionHandler: @escaping () -> Void
+          ) {
+            completionHandler()
+          }
+    override func application(
+          _ application: UIApplication,
+          didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+        ) {
+          Messaging.messaging().apnsToken = deviceToken
+            Messaging.messaging().subscribe(toTopic: "new_events")
+        }
 }
