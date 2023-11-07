30a1defde6cc3cc49abff481f690d4de40c39dbb
Olivia <larsonog@hendrix.edu>
2023-11-07 15:07:24 -0600

    Working on disable alert box

--- lib/widgets/resource_button.dart
+++ lib/widgets/resource_button.dart
@@ -70,19 +70,7 @@ class ResourceButton extends StatelessWidget {
               Icon(icon, size: 60, color: Theme.of(context).iconTheme.color),
           onTap: () => showDialog<String>(
             context: context,
-            builder: (BuildContext context) => AlertDialog(
-              title: const Text('This is an external link. Continue?'),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () => Navigator.pop(context, 'Cancel'),
-                  child: const Text('Cancel'),
-                ),
-                TextButton(
-                  onPressed: () => _tryLaunchUrl(url),
-                  child: const Text('OK'),
-                ),
-              ],
-            ),
+            builder: (BuildContext context) => AlertDialogBox()
           )
 /*            _tryLaunchUrl(url).then(
                 (value) => ScaffoldMessenger.of(context).showSnackBar(SnackBar(
@@ -96,3 +84,78 @@ class ResourceButton extends StatelessWidget {
     );
   }
 }
+
+class AlertDialogBox extends StatefulWidget {
+  const AlertDialogBox({super.key});
+
+  @override
+  State<AlertDialogBox> createState() => _AlertDialogBoxState();
+}
+
+class _AlertDialogBoxState extends State<AlertDialogBox> {
+
+  @override
+  Widget build(BuildContext context) {
+    return(
+      AlertDialog(
+        title: const Text('This is an external link. Continue?'),
+        actions: <Widget>[
+          Row(children: [
+
+          TextButton(
+            onPressed: () => Navigator.pop(context, 'Cancel'),
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () => _tryLaunchUrl("www.google.com"),
+            child: const Text('OK'),
+          ),
+          
+          Text("Disable?"),
+          CheckboxExample()
+          ],),
+        ],
+      )
+
+    );
+  }
+}
+
+class CheckboxExample extends StatefulWidget {
+  const CheckboxExample({super.key});
+
+  @override
+  State<CheckboxExample> createState() => _CheckboxExampleState();
+}
+
+class _CheckboxExampleState extends State<CheckboxExample> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Colors.grey;
+    }
+
+    return Checkbox(
+      checkColor: Colors.white,
+      fillColor: MaterialStateProperty.resolveWith(getColor),
+      value: isChecked,
+      onChanged: (bool? value) {
+        setState(() {
+          isChecked = value!;
+        });
+      },
+    );
+  }
+}
+
+
